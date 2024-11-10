2ff37993198c7877f9c13d2f648b9e9bb293912a
Birk Skyum <birk.skyum@pm.me>
2024-11-10 16:51:42 +0100

    fix proj

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,26 +14,38 @@ function generateLatLngGrid(spacing: number) {
 }
 
 export default function Home() {
-  var firstProjection = 'PROJCS["NAD83 / Massachusetts Mainland",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",42.68333333333333],PARAMETER["standard_parallel_2",41.71666666666667],PARAMETER["latitude_of_origin",41],PARAMETER["central_meridian",-71.5],PARAMETER["false_easting",200000],PARAMETER["false_northing",750000],AUTHORITY["EPSG","26986"],AXIS["X",EAST],AXIS["Y",NORTH]]';
-  var secondProjection = "+proj=gnom +lat_0=90 +lon_0=0 +x_0=6300000 +y_0=6300000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
-  //I'm not going to redefine those two in latter examples.
-  const firstProjCoords = [-122.305887, 58.9465872]
-  const secondProjCoords = proj4(firstProjection,secondProjection,firstProjCoords);
-
-  const equalEarthProjection = "+proj=eqearth +datum=WGS84 +units=m +no_defs";
+  
+  const equalEarthProjection = 'PROJCS["World_Equal_Earth",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Equal_Earth"],PARAMETER["false_easting",0],PARAMETER["false_northing",0],PARAMETER["longitude_of_center",0],PARAMETER["latitude_of_center",0],UNIT["Meter",1]]';
+  const webMercatorProjection = 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["Meter",1],AUTHORITY["EPSG","3857"]]';
+  const verticalPerspectiveProjection = '+proj=geos +lat_0=0 +lon_0=0 +h=35785831 +datum=WGS84 +units=m +no_defs';
+  
   const gridSpacing = 10; // degrees
   const latLngGrid = generateLatLngGrid(gridSpacing);
-  const [useEqualEarth, setUseEqualEarth] = createSignal(true);
+  const [projectionType, setProjectionType] = createSignal("EqualEarth");
   const [projectedGrid, setProjectedGrid] = createSignal(latLngGrid.map(coords => proj4('EPSG:4326', equalEarthProjection, coords)));
 
-  const toggleProjection = () => {
-    setUseEqualEarth(!useEqualEarth());
-    const newProjection = useEqualEarth() ? secondProjection : equalEarthProjection;
-    setProjectedGrid(latLngGrid.map(coords => proj4('EPSG:4326', newProjection, coords)));
+  const updateProjection = (type) => {
+    setProjectionType(type);
+    let newProjection;
+    switch (type) {
+      case "WebMercator":
+        newProjection = webMercatorProjection;
+        break;
+      case "VerticalPerspective":
+        newProjection = verticalPerspectiveProjection;
+        break;
+      case "EqualEarth":
+      default:
+        newProjection = equalEarthProjection;
+        break;
+    }
+    setProjectedGrid(latLngGrid.map(coords => {
+      const [x, y] = proj4('EPSG:4326', newProjection, coords);
+      return [isNaN(x) ? 0 : x, isNaN(y) ? 0 : y];
+    }));
   };
 
-  console.log('first', firstProjCoords); 
-  console.log('second', secondProjCoords); 
+  
   console.log('Lat/Lng Grid', latLngGrid);
   console.log('EqualEarth Grid', projectedGrid());
 
@@ -42,12 +54,21 @@ export default function Home() {
       
       <p>
         
-        {useEqualEarth()? 'Showing Equal Earth':'Showing LatLng'}
+        {'Showing ' + projectionType()}
 
       </p>
-      <button onClick={toggleProjection}>
-        {'Toggle proj'}
+
+      <button onClick={() => updateProjection("EqualEarth")}>
+        Equal Earth
       </button>
+      <button onClick={() => updateProjection("WebMercator")}>
+        Web Mercator
+      </button>
+      <button onClick={() => updateProjection("VerticalPerspective")}>
+        Vertical Perspective
+      </button>
+
+      <br />
       <svg width="800" height="400" viewBox="-20000000 -10000000 40000000 20000000" style={{ border: "1px solid black" }}>
         {projectedGrid().map(([x, y]) => (
           <circle cx={x} cy={-y} r="200000" fill="red" />
