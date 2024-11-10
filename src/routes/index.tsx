88ed7134d86059784b0f69b45ef8a2297e401cd6
Birk Skyum <birk.skyum@pm.me>
2024-11-10 18:23:47 +0100

    lerp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,13 @@
 import proj4 from "proj4";
 import { createSignal, onCleanup } from "solid-js";
 
+
+// const myClone = structuredClone;
+
+const myClone = (obj)=>{
+  return JSON.parse(JSON.stringify(obj));
+}
+
 function generateLatLngGrid(spacing: number) {
   const points = [];
   for (let lat = -90; lat <= 90; lat += spacing) {
@@ -14,7 +21,12 @@ function generateLatLngGrid(spacing: number) {
 enum ProjectionType {
   EqualEarth = "Equal Earth",
   WebMercator = "Web Mercator",
-  VerticalPerspective = "Vertical Perspective"
+  VerticalPerspective = "Vertical Perspective",
+  NaturalEarth = "Natural Earth"
+}
+
+function clampLat(lat: number): number {
+  return Math.max(-85, Math.min(85, lat));
 }
 
 export default function Home() {
@@ -26,6 +38,7 @@ export default function Home() {
   const equalEarthProjection = 'PROJCS["World_Equal_Earth",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Equal_Earth"],PARAMETER["false_easting",0],PARAMETER["false_northing",0],PARAMETER["longitude_of_center",0],PARAMETER["latitude_of_center",0],UNIT["Meter",1]]';
   const webMercatorProjection = 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["Meter",1],AUTHORITY["EPSG","3857"]]';
   const verticalPerspectiveProjection = '+proj=geos +lat_0=0 +lon_0=0 +h=35785831 +datum=WGS84 +units=m +no_defs';
+  // const naturalEarthProjection = '+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs +type=crs'
 
   
   const gridSpacing = 10; // degrees
@@ -33,14 +46,16 @@ export default function Home() {
 
   
   const equalEarthCoords = latLngGrid.map(coords => proj4('EPSG:4326', equalEarthProjection, coords));
-  const webMercatorCoords = latLngGrid.map(coords => proj4('EPSG:4326', webMercatorProjection, coords));
+  const webMercatorCoords = latLngGrid.map(coords => {
+    const [lng, lat] = coords;
+    // const clampedLat = clampLat(lat);
+    return proj4('EPSG:4326', webMercatorProjection, [lng, lat]);
+  });
   const verticalPerspectiveCoords = latLngGrid.map(coords => proj4('EPSG:4326', verticalPerspectiveProjection, coords));
   
 
-  
-
   const [projectionType, setProjectionType] = createSignal(ProjectionType.WebMercator);
-  const [projectedGrid, setProjectedGrid] = createSignal(structuredClone(webMercatorCoords));
+  const [projectedGrid, setProjectedGrid] = createSignal(myClone(webMercatorCoords));
   const [animationFrame, setAnimationFrame] = createSignal<number | undefined>(undefined);
   
 
@@ -70,20 +85,24 @@ export default function Home() {
     let endGrid;
     switch (type) {
       case ProjectionType.WebMercator:
-        endGrid = structuredClone(webMercatorCoords);
+        endGrid = myClone(webMercatorCoords);
         
         break;
       case ProjectionType.VerticalPerspective:
-        endGrid = structuredClone(verticalPerspectiveCoords);
+        endGrid = myClone(verticalPerspectiveCoords);
         
         break;
       case ProjectionType.EqualEarth:
-      default:
-        endGrid = structuredClone(equalEarthCoords);
+        endGrid = myClone(equalEarthCoords);
+        
+        break;
+      // case ProjectionType.NaturalEarth:
+      // default:
+      //   endGrid = myClone(naturalEarthCoords);
         
         break;
     }
-    animateProjection(structuredClone(projectedGrid()), structuredClone(endGrid), 0);
+    animateProjection(myClone(projectedGrid()), myClone(endGrid), 3000);
   };
 
   onCleanup(() => {
@@ -114,12 +133,26 @@ export default function Home() {
       <button onClick={() => updateProjection(ProjectionType.VerticalPerspective)}>
         Vertical Perspective
       </button>
+      {/* <button onClick={() => updateProjection(ProjectionType.NaturalEarth)}>
+        Natural Earth
+      </button> */}
 
       <br />
       <svg width="800" height="800" viewBox={"-20000000 -10000000 40000000 20000000"} style={{ border: "1px solid black" }}>
-        {projectedGrid().map(([x, y]) => (
-          <circle cx={x} cy={-y} r="200000" fill="red" />
-        ))}
+        {projectedGrid().map(([x, y]) => {
+
+
+
+
+          if (x == undefined  || y == undefined) {
+            return <circle cx={x} cy={-y} r="400000" fill="blue" />
+          } else {
+            return <circle cx={x} cy={-y} r="200000" fill="red" />
+          }
+         
+        })}
+
+
       </svg>
     </main>
   );
